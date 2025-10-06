988736c7ed075dd54f5e97f0782f012c2c70eacf
nvd11 <nvd11@163.com>
2025-10-06 10:50:03 +0800

    t

--- src/configs/config.py
+++ src/configs/config.py
@@ -14,12 +14,9 @@ sys.path.append(project_path)
 
 
 # setup logs path
-log_format = (
-    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
-    "<level>{level: <8}</level> | "
-    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
-)
-logger.add(os.path.join(project_path, "logs", "app.log"), format=log_format)
+
+
+logger.add(os.path.join(project_path, "logs", "app.log"))
 
 logger.info("basic setup done")
 
@@ -29,4 +26,4 @@ yaml_configs = None
 with open(os.path.join(project_path, "src", "configs", "config_dev.yaml")) as f:
     yaml_configs = yaml.load(f, Loader=yaml.FullLoader)
 
-logger.info("all configs loaded")
+logger.info("all configs loaded")sss
